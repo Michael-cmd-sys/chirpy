9a349d410bf12811e10341c908a680540ae4b68b
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2026-04-07 19:38:34 +0000

    implement stateful Handlers

--- main.go
+++ main.go
@@ -1,30 +1,71 @@
-package main 
+package main
 
 import (
 	//"fmt"
-	"net/http"
+	"fmt"
 	"log"
+	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache")
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) MetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	_, err := fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (cfg *apiConfig) MetricsResetHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+}
+
+func healthHander(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "OK")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
 func main() {
+	const FILEROOT = "./static"
+	const PORT = ":8080"
+
+	// Server config setup
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	// Create a server instance handler
 	mux := http.NewServeMux()
 
-	// Static file server instance
-	fileServer := http.FileServer(http.Dir("./static"))
-	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(FILEROOT)))))
 
-	// Serve static html file to users
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./static/index.html")
-	})
+	mux.HandleFunc("/metrics", apiCfg.MetricsHandler)
+
+	mux.HandleFunc("/reset", apiCfg.MetricsResetHandler)
+
+	mux.HandleFunc("/healthz", healthHander)
 
 	srv := &http.Server{
-		Addr   : ":8080",
+		Addr:    PORT,
 		Handler: mux,
 	}
 
 	// Start the server
-	log.Println("Server started on port http://localhost:8080...")
+	log.Printf("Server started on port http://localhost%s\n", PORT)
 	log.Fatal(srv.ListenAndServe())
 }
